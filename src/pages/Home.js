f9cbdb8bfd190b1064e4f50cdd0414787b35ca0a
ljubebiza <srbija.majka@gmail.com>
2022-05-23 14:45:14 +0200

    new change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -61,7 +61,6 @@ export default function Home({ articles, setArticles }) {
           setTitle={setTitle}
         />
       )}
-
       <form className="input-form">
         <InputField
           fieldPlaceholder={"Title"}
